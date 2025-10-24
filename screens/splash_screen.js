de65ef64e382d1d65ca1398c28340183dd5d6e13
xsamtech <124665689+xsamtech@users.noreply.github.com>
2025-10-24 04:52:12 +0100

    n

--- screens/splash_screen.js
+++ screens/splash_screen.js
@@ -3,12 +3,13 @@
  * @see https://team.xsamtech.com/xanderssamoth
  */
 import React from 'react';
-import { Image, View } from 'react-native';
+import { View } from 'react-native';
+import GifImage from '@lowkey/react-native-gif';
 
 const SplashScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#171614' }}>
-      <Image source={require('../assets/img/splash.gif')} style={{ width: '100%', height: '100%' }} />
+      <GifImage source={require('../assets/img/splash.gif')} style={{ width: '100%', height: '100%' }} resizeMode={'cover'} />;
     </View>
   );
 };
